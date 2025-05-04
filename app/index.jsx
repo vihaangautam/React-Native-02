b12877b35eb0d07dd15de803d931911285c7f5de
Vihaan Gautam <vihaangautam04@gmail.com>
2025-05-04 19:06:42 +0530

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, ImageBackground} from 'react-native'
 import React from 'react'
 import icedCoffeeImg from "@/assets/images/iced-coffee.png"
+import {Link}from 'expo-router'
 
 const app = () => {
   return (
@@ -10,7 +11,8 @@ const app = () => {
         resizeMode="Cover"
         style={styles.image}
         >
-      <Text style={styles.text}>Coffee Shop</Text>
+      <Text style={styles.title}>Coffee Shop</Text>
+      <Link href='/explore' style ={styles.link}>Explore</Link>
       </ImageBackground>
     </View>
   )
@@ -30,11 +32,22 @@ const styles= StyleSheet.create({
     resizeMode:'cover',
     justifyContent:'center'
   },
-  text:{
+  title:{
     color: 'white',
     fontSize:42,
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0.5)',
-  }
+    marginBottom: 120,
+  },
+  link:{
+    color: 'white',
+    fontSize:42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecorationLine:'underline',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 4,
+  },
+
 })
