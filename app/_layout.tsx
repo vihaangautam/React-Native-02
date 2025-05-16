e24de50c0a499c1e4aa2ca0507f9465a3641b643
Vihaan Gautam <vihaangautam04@gmail.com>
2025-05-16 22:55:44 +0530

    removed themes and added appearance

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
+import { Appearance } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
